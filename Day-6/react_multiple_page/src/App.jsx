afb9c494d8c6df52a5a01fb8a385b88ab3546e0c
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-07-27 11:58:28 +0600

    done

--- Day-6/react_multiple_page/src/App.jsx
+++ Day-6/react_multiple_page/src/App.jsx
@@ -1,15 +1,27 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About";
+import Services from "./components/Service/Service";
+import Contact from "./components/Contact/Contact";
+import Pricing from "./components/Pricing/Pricing";
 
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-
-function App() {
+const App = () => {
   return (
     <div>
+      
       <Navbar />
-      <Outlet />
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/home" element={<Home></Home>} />
+        <Route path="/service" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
